390c5421f6eb888beb22b993ddb1c41cd1a4d68f
Erik Söderberg <Zednosius@gmail.com>
2017-12-19 19:31:03 +0100

    Implement playing and stopping sound. Playing a new sound stops the old one. Also improve button text

--- soundboard.py
+++ soundboard.py
@@ -3,21 +3,43 @@ from flask import request, render_template
 from playsound import playsound
 from flask import jsonify
 import os
+import vlc
+import time
 
 app = Flask(__name__)
 
 #Gets filenames of sounds
 sound_filenames = list(os.walk("sounds"))[0][2]
-cache = {"sounds" : list(zip(map(lambda x: os.path.splitext(x)[0], sound_filenames), sound_filenames))}
-print("Cache:", cache)
+zipped_name_filenames = list(zip(map(lambda x: os.path.splitext(x)[0], sound_filenames), sound_filenames))
+cache = {value: value for (key,value) in zipped_name_filenames}
+cache["sounds"] = zipped_name_filenames
+instance = vlc.Instance()
+player = instance.media_player_new()
+
+print("Player type",type(player))
 
 @app.route("/", methods=["POST","GET"])
 def hello():
+    global player
     print(request)
     if request.method == "POST":
-        print("Data",request.get_json())
-        print("It did something!")
-        playsound("sounds/beep-30b.mp3")
-        return jsonify(success=True)
+        data = request.get_json()
+        print("Data",data)
+
+        if player is not None and data.get("stop",False):
+            player.stop()
+            return jsonify(success=True)
+        elif data["mp3"] in cache:
+            player.set_media(instance.media_new("sounds/"+data["mp3"]))
+            
+            # time.sleep(1)
+            player.play()
+            return jsonify(success=True)
+        else:
+            return jsonify(success=False)
+        
     else:
-        return render_template("index.html", sounds=cache["sounds"])
+        return render_template("index.html", sounds=map(lambda x: (x[0].replace("_"," "),x[1]), cache["sounds"]))
+
+if __name__ == '__main__':
+    app.run(threaded=True, host="0.0.0.0",debug=False)
